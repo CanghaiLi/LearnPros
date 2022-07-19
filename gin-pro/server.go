eb6f4d79b5ed1abc8e614b88eaf5e864b1236bb1
LICANGHAI <liningjnu@gmail.com>
2022-07-19 19:10:09 +0800

    gin: init project

--- gin-pro/server.go
+++ gin-pro/server.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
 type User struct {
 	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Age  int    `json:"age" binding:"required,range18"`
 	Sex  bool   `json:"sex"`
+}
+
+func ccc[T int](p T) T {
+	return p
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/test", func(c *gin.Context) {
+
+		// 注册检验函数
+		if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+			v.RegisterValidation("range18", range18)
+		}
+
 		var u User
 		err := c.ShouldBind(&u)
 		if err != nil {
@@ -27,4 +39,12 @@ func main() {
 		}
 	})
 	r.Run()
+}
+
+func range18(fl validator.FieldLevel) bool {
+	if fl.Field().Interface().(int) >= 18 {
+		return true
+	}
+	return false
+
 }
