257d036c38a469ff78a338247402d559b39cafa7
LICANGHAI <liningjnu@gmail.com>
2022-07-19 16:57:57 +0800

    gin: init project

--- gin-pro/server.go
+++ gin-pro/server.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	Sex  bool   `json:"sex"`
+}
 
 func main() {
-	fmt.Println(123)
+	r := gin.Default()
+	r.POST("/test", func(c *gin.Context) {
+		var u User
+		err := c.ShouldBind(&u)
+		if err != nil {
+			fmt.Println(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": fmt.Sprintf("%s", err),
+			})
+		} else {
+			c.JSON(http.StatusOK, u)
+		}
+	})
+	r.Run()
 }
