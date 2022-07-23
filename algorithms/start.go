1474b625028bf604cb2598a7f1331140c959fab7
LICANGHAI <liningjnu@gmail.com>
2022-07-23 23:04:53 +0800

    gin: add one project

--- algorithms/start.go
+++ algorithms/start.go
@@ -1,7 +1,15 @@
 package main
 
-var Arr = []int{6, 1, 3, 4, 10, 4, 5}
+var Arr = []int{6, 1, -3, 4, -9, 4, 5, 2}
 
 func main() {
 
+}
+
+func ThreeSum(arr []int) [][]int {
+	var res [][]int
+	if arr == nil || len(arr) < 3 {
+		return res
+	}
+	return res
 }
